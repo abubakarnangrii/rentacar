910a1f1668ff14211ca5eee26a17cb9d7ad60d63
abubakarnangrii <abubakarnangri@gmail.com>
2023-12-17 03:39:17 +0500

    NavBar and Footer Updated

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carousel.css'; // Import your CSS file
 
 const Carousel = () => {
@@ -13,18 +13,42 @@ const Carousel = () => {
     };
 
     const images = [
-        'assets/images/slider/slide_1.jpg',
-        'assets/images/slider/slide_2.jpg',
-        'assets/images/slider/slide_3.jpg',
-        // Add more image paths as needed
+        {
+            path: 'assets/images/slider/slide_1.jpg',
+            title: 'Title 1',
+            paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit vel urna vehicula fringilla.'
+        },
+        {
+            path: 'assets/images/slider/slide_2.jpg',
+            title: 'Title 2',
+            paragraph: 'Sed ac quam ut libero fermentum euismod. Nulla facilisi. Vestibulum eu tristique velit.'
+        },
+        {
+            path: 'assets/images/slider/slide_3.jpg',
+            title: 'Title 3',
+            paragraph: 'Praesent bibendum nunc ut odio vestibulum, ac volutpat sapien semper. Suspendisse potenti.'
+        },
+        // Add more image paths, titles, and paragraphs as needed
     ];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            nextSlide();
+        }, 3000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [currentIndex]);
 
     return (
         <div className="carousel-container">
             <div className="carousel-wrapper" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
                 {images.map((image, index) => (
                     <div key={index} className="carousel-item">
-                        <img src={image} alt={`Slide ${index + 1}`} />
+                        <img src={image.path} alt={`Slide ${index + 1}`} />
+                        <h2 className="image-title">{image.title}</h2>
+                        <p className="image-paragraph">{image.paragraph}</p>
                     </div>
                 ))}
             </div>
